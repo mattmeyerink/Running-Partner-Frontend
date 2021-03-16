e36747f98e87c9f711960a56907daa1d379719a8
mattmeyerink <meyerink@umich.edu>
2021-03-15 21:47:14 -0400

    Adding prettier to home screen

--- src/views/home.js
+++ src/views/home.js
@@ -7,7 +7,6 @@ import RunEntry from '../components/run_entry';
 import Config from '../config';
 import '../index.css';
 
-// view for the homepage
 class Home extends Component {
     constructor(props) {
         super(props);
@@ -21,18 +20,18 @@ class Home extends Component {
         }
     }
     
-    // Make initial API Calls for weather data, today's run, and run input.
     componentDidMount() {
         // Set the users page to blank to unhighlight any nav bar tabs
         this.props.setCurrentPage("");
 
+        // Ensure user authenticated before loading data for weather, quote and runs
         if (this.props.userAuthenticated) {
-            // Fetch the weather data (hind sight I should move this to the weatherWidget component)
             const myHeaders = new Headers({
                 'Content-Type': 'application/json',
                 'Authorization': 'Bearer ' + this.props.userData.token,
             });
 
+            // Fetch the weather data
             fetch(Config.rpAPI + `/weather/${this.props.userData.city}`, {
                 method: 'GET',
                 headers: myHeaders
@@ -42,17 +41,18 @@ class Home extends Component {
                 .catch(error => console.error(error))
             }
 
+            // Note that the api changed so until I get another one I am commenting the fetch out
             // Fetch motivational quote
-            fetch("https://type.fit/api/quotes")
-                .then(response => response.json())
-                .then(data => {
-                    const randomNum = Math.floor(Math.random() * data.length);
-                    this.setState({
-                        motivationalQuoteText: data[randomNum].text,
-                        motivationalQuoteAuthor: data[randomNum].author
-                    })
-                })
-                .catch(error => console.error(error))
+            // fetch("https://type.fit/api/quotes")
+            //     .then(response => response.json())
+            //     .then(data => {
+            //         const randomNum = Math.floor(Math.random() * data.length);
+            //         this.setState({
+            //             motivationalQuoteText: data[randomNum].text,
+            //             motivationalQuoteAuthor: data[randomNum].author
+            //         })
+            //     })
+            //     .catch(error => console.error(error))
 
 
             // Set the greeting message
@@ -78,58 +78,58 @@ class Home extends Component {
         return (
             <div className="home_page">
                 {this.props.userAuthenticated ?
-                <React.Fragment>
-                    <div className="row justify-content-center home_widgets">
-                        <h1 className="white_text">{this.state.greeting} {first_name}!</h1>
-                    </div>
-                    
-
-                    {this.state.loading ?
-                    <div className="row justify-content-center">
-                        <h1 className="white_text">Loading...</h1>
-                    </div>
-                    :
                     <React.Fragment>
-                        {/*
-                        Note Temporarily pulling motivaitonal quote seciton
-                        <div className="row justify-content-center">
-                            <div className="col-md-8">
-                                <div className="row justify-content-center home_widgets">
-                                    <div className="col">
+                        <div className="row justify-content-center home_widgets">
+                            <h1 className="white_text">{this.state.greeting} {first_name}!</h1>
+                        </div>
+                        
+
+                        {this.state.loading ?
+                            <div className="row justify-content-center">
+                                <h1 className="white_text">Loading...</h1>
+                            </div>
+                        :
+                            <React.Fragment>
+                                {/*
+                                Note Temporarily pulling motivaitonal quote seciton
+                                <div className="row justify-content-center">
+                                    <div className="col-md-8">
+                                        <div className="row justify-content-center home_widgets">
+                                            <div className="col">
+                                                <div className="row justify-content-center">
+                                                    <h3 className="quote">{this.state.motivationalQuoteText}</h3>
+                                                </div>
+                                            </div>
+                                        </div>
                                         <div className="row justify-content-center">
-                                            <h3 className="quote">{this.state.motivationalQuoteText}</h3>
+                                            {this.state.motivationalQuoteAuthor === null?
+                                            <h3 className="quote">~ unknown</h3>
+                                            :
+                                            <h3 className="quote">~{this.state.motivationalQuoteAuthor}</h3>
+                                            }
                                         </div>
                                     </div>
                                 </div>
-                                <div className="row justify-content-center">
-                                    {this.state.motivationalQuoteAuthor === null?
-                                    <h3 className="quote">~ unknown</h3>
-                                    :
-                                    <h3 className="quote">~{this.state.motivationalQuoteAuthor}</h3>
-                                    }
+                                */}
+                                
+                                <div className="row justify-content-center home_widgets">
+                                    <div className="col-md-3 widget_spacing">
+                                        <WeatherWidget city={city} state={state} weatherData={this.state.weatherData} />
+                                    </div>
+                                    <div className="col-md-4 widget_spacing">
+                                        <TodaysRun userData={this.props.userData}/>
+                                    </div>
+                                    <div className="col-md-3 widget_spacing">
+                                        <RunEntry user_id={id} city={city} state={state} userData={this.props.userData}/>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
-                        */}
-                        
-                        <div className="row justify-content-center home_widgets">
-                            <div className="col-md-3 widget_spacing">
-                                <WeatherWidget city={city} state={state} weatherData={this.state.weatherData} />
-                            </div>
-                            <div className="col-md-4 widget_spacing">
-                                <TodaysRun userData={this.props.userData}/>
-                            </div>
-                            <div className="col-md-3 widget_spacing">
-                                <RunEntry user_id={id} city={city} state={state} userData={this.props.userData}/>
-                            </div>
-                        </div>
-                    </React.Fragment>
-                    }
-                </React.Fragment> 
-                :
-                <React.Fragment>
-                    <Redirect to="/login" />
-                </React.Fragment>
+                            </React.Fragment>
+                        }
+                        </React.Fragment> 
+                    :
+                        <React.Fragment>
+                            <Redirect to="/login" />
+                        </React.Fragment>
                 }
             </div>
         )
