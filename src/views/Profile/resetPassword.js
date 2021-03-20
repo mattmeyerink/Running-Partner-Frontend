2bd2ff8a2480874d48e482246d4a8090459560a1
mattmeyerink <meyerink@umich.edu>
2021-03-20 09:51:38 -0400

    Creating page to reset password

---
+++ src/views/Profile/resetPassword.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import '../../index.css';
+
+class ResetPassword extends Component {
+    constructor() {
+        super();
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <div className="row justify-content-center">
+                    <h1>Reset Password</h1>
+                </div>
+            </React.Fragment>
+        );
+    }
+}
+
+export default ResetPassword;
