acf45bc67ced23c75e78f2942f22c2c53ebec8d3
mattmeyerink <meyerink@umich.edu>
2021-03-26 22:50:06 -0400

    Adding confirm password reset page

---
+++ src/views/Authentication/confirmPasswordReset.js
@@ -0,0 +1,18 @@
+import React, { Comopnent } from 'react';
+
+class ConfirmPasswordReset extends Component {
+    constructor() {
+        super();
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <div className="row justify-content-center">
+                    <h1>Confirm Password Reset</h1>
+                </div>
+            </React.Fragment>
+        );
+    }
+}
