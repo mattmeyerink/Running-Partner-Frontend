fe9ea77b93d151b33b75f54f955b97e768998142
mattmeyerink <meyerink@umich.edu>
2021-03-27 12:22:36 -0400

    Adding handlesubmit logic and API call

--- src/views/Authentication/confirmPasswordReset.js
+++ src/views/Authentication/confirmPasswordReset.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Config from "../../config";
 import "../../index.css";
 
 class ConfirmPasswordReset extends Component {
@@ -6,6 +7,8 @@ class ConfirmPasswordReset extends Component {
     super();
     this.state = {
       email: "",
+      emailSent: false,
+      emailNotFound: false,
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -19,8 +22,26 @@ class ConfirmPasswordReset extends Component {
     this.setState({ [name]: value });
   }
 
-  handleSubmit() {
-    console.log("whats up");
+  handleSubmit(event) {
+    const emailData = {
+      email: this.state.email,
+    };
+
+    fetch(Config.rpAPI + "/authentication/initiate_password_reset", {
+      method: "POST",
+      body: JSON.stringify(emailData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((response) => {
+      if (response.status === 200) {
+        this.setState({ emailSent: true });
+        this.setState({ emailNotFound: false });
+      } else if (response.status === 404) {
+        this.setState({ emailNotFound: true });
+      }
+    });
+    event.preventDefault();
   }
 
   render() {
@@ -29,26 +50,46 @@ class ConfirmPasswordReset extends Component {
         <div className="row justify-content-center">
           <h1 className="white_text">Confirm Password Reset</h1>
         </div>
-        <div className="row justify-content-center">
-          <h4 className="white_text">
-            Enter your email to recieve a link to reset your password
-          </h4>
-        </div>
-        <div className="row justify-content-center">
-          <div className="col-md-6">
-            <form onSubmit={this.handleSubmit}>
-              <input
-                type="text"
-                name="email"
-                value={this.state.email}
-                className="form-control form_spacing"
-                onChange={this.handleChange}
-                placeholder="Email"
-              />
-              <input type="submit" className="btn btn-success form-control" />
-            </form>
-          </div>
-        </div>
+        {this.state.emailSent ? (
+          <React.Fragment>
+            <h4 className="white_text">Email Sent!</h4>
+          </React.Fragment>
+        ) : (
+          <React.Fragment>
+            <div className="row justify-content-center">
+              <h4 className="white_text">
+                Enter your email to recieve a link to reset your password
+              </h4>
+            </div>
+            {this.state.emailNotFound ? (
+              <React.Fragment>
+                <div className="row justify-content-center">
+                  <h4 className="white_text">Email Not Found</h4>
+                </div>
+              </React.Fragment>
+            ) : (
+              <React.Fragment></React.Fragment>
+            )}
+            <div className="row justify-content-center">
+              <div className="col-md-6">
+                <form onSubmit={this.handleSubmit}>
+                  <input
+                    type="text"
+                    name="email"
+                    value={this.state.email}
+                    className="form-control form_spacing"
+                    onChange={this.handleChange}
+                    placeholder="Email"
+                  />
+                  <input
+                    type="submit"
+                    className="btn btn-success form-control"
+                  />
+                </form>
+              </div>
+            </div>
+          </React.Fragment>
+        )}
       </React.Fragment>
     );
   }
