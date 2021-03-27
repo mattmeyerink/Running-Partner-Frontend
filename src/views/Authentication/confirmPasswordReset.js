3d9b310bd94041604e408204fbc78557c95d4277
mattmeyerink <meyerink@umich.edu>
2021-03-27 11:01:36 -0400

    Adding handle submit skeleton

--- src/views/Authentication/confirmPasswordReset.js
+++ src/views/Authentication/confirmPasswordReset.js
@@ -9,13 +9,18 @@ class ConfirmPasswordReset extends Component {
     };
 
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
-      const target = event.target;
-      const { name, value } = target;
+    const target = event.target;
+    const { name, value } = target;
 
-      this.setState({ [name]: value });
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit() {
+    console.log("whats up");
   }
 
   render() {
@@ -25,11 +30,13 @@ class ConfirmPasswordReset extends Component {
           <h1 className="white_text">Confirm Password Reset</h1>
         </div>
         <div className="row justify-content-center">
-            <h4 className="white_text">Enter your email to recieve a link to reset your password</h4>
+          <h4 className="white_text">
+            Enter your email to recieve a link to reset your password
+          </h4>
         </div>
         <div className="row justify-content-center">
           <div className="col-md-6">
-            <form>
+            <form onSubmit={this.handleSubmit}>
               <input
                 type="text"
                 name="email"
