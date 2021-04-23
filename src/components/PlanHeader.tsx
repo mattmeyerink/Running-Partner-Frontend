c13ad0f6c9198805f954698c97c3a107789457d2
Matthew Meyerink <meyerink@umich.edu>
2021-04-23 15:08:05 -0700

    Refactoring to TS

--- src/components/PlanHeader.tsx
+++ src/components/PlanHeader.tsx
@@ -2,24 +2,33 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "./components.css";
 
+interface PlanHeaderProps {
+  id: number;
+  race_name: string;
+  difficulty: string;
+  plan_length: number;
+  frequency: number;
+}
+
 /**
  * Class to display the header information for pre made training plans
  */
 class PlanHeader extends Component {
   render() {
+    const {race_name, difficulty, plan_length, frequency, id } = this.props as PlanHeaderProps;
     return (
       <React.Fragment>
         <div className="col-md-8 border border-secondary training_plan_card">
-          <Link to={`/training_plans/${this.props.id}`}>
+          <Link to={`/training_plans/${id}`}>
             <div className="row">
               <div className="col-md-6">
                 <strong>
-                  {this.props.race_name} - {this.props.difficulty}
+                  {race_name} - {difficulty}
                 </strong>
               </div>
               <div className="col-md-3 offset-3">
                 <p>
-                  {this.props.plan_length} weeks - {this.props.frequency}{" "}
+                  {plan_length} weeks - {frequency}{" "}
                   runs/week
                 </p>
               </div>
