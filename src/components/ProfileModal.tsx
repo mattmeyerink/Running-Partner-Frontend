20374e4844af1beb1727378e4238e62568a55a9d
Matthew Meyerink <meyerink@umich.edu>
2021-06-17 22:07:23 -0400

    Improving profile form formatting

--- src/components/ProfileModal.tsx
+++ src/components/ProfileModal.tsx
@@ -40,6 +40,7 @@ class ProfileModal extends Component<ProfileModalProps, ProfileModalState> {
       loading: false,
     };
 
+    this.beginEditing = this.beginEditing.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
@@ -56,8 +57,15 @@ class ProfileModal extends Component<ProfileModalProps, ProfileModalState> {
    * Submit the edit profile form and exit editing mode
    */
   handleSubmit() {
-    console.log('Profile Changes Submitted!');
+    console.log("Profile Changes Submitted!");
     this.setState({ editing: false });
+  }
+
+  /*
+   * Set state to begin editing the user's data
+   */
+  beginEditing() {
+    this.setState({ editing: true });
   }
 
   render() {
@@ -69,83 +77,63 @@ class ProfileModal extends Component<ProfileModalProps, ProfileModalState> {
         onHide={this.props.handleProfileModalClose}
       >
         <Modal.Header closeButton>
-          <Modal.Title>{first_name}'s Profile</Modal.Title>
+          <Modal.Title>My Profile</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <React.Fragment>
             {this.state.editing ? (
               <React.Fragment>
-                <div className="row justify-content-center">
-                  <h1 className="white_text">Edit Account Information</h1>
-                </div>
-                <div className="row justify-content-center">
-                  <div className="col-md-6 background_color edit_profile_padding">
-                    <form onSubmit={this.handleSubmit}>
-                      <input
-                        type="text"
-                        name="firstName"
-                        value={this.state.firstName}
-                        onChange={this.handleChange}
-                        className="form-control form_spacing"
-                        placeholder="First Name"
-                      />
-                      <input
-                        type="text"
-                        name="lastName"
-                        value={this.state.lastName}
-                        onChange={this.handleChange}
-                        className="form-control form_spacing"
-                        placeholder="Last Name"
-                      />
-                      <input
-                        type="text"
-                        name="username"
-                        value={this.state.username}
-                        onChange={this.handleChange}
-                        className="form-control form_spacing"
-                        placeholder="Username"
-                      />
-                      <input
-                        type="text"
-                        name="email"
-                        value={this.state.email}
-                        onChange={this.handleChange}
-                        className="form-control form_spacing"
-                        placeholder="Email"
-                      />
-                      <input
-                        type="text"
-                        name="city"
-                        value={this.state.city}
-                        onChange={this.handleChange}
-                        className="form-control form_spacing"
-                        placeholder="City"
-                      />
-                      <select
-                        name="state"
-                        value={this.state.state}
-                        onChange={this.handleChange}
-                        className="form-control form_spacing"
-                      >
-                        <StatesForm />
-                      </select>
-                      <p className="warning_text">{this.state.warning}</p>
-                      <button
-                        type="submit"
-                        className="btn btn-success form-control"
-                      >
-                        Submit{" "}
-                        {this.state.loading && (
-                          <Spinner
-                            animation="border"
-                            variant="light"
-                            size="sm"
-                          />
-                        )}
-                      </button>
-                    </form>
-                  </div>
-                </div>
+                <form onSubmit={this.handleSubmit}>
+                  <input
+                    type="text"
+                    name="firstName"
+                    value={this.state.firstName}
+                    onChange={this.handleChange}
+                    className="form-control form_spacing"
+                    placeholder="First Name"
+                  />
+                  <input
+                    type="text"
+                    name="lastName"
+                    value={this.state.lastName}
+                    onChange={this.handleChange}
+                    className="form-control form_spacing"
+                    placeholder="Last Name"
+                  />
+                  <input
+                    type="text"
+                    name="username"
+                    value={this.state.username}
+                    onChange={this.handleChange}
+                    className="form-control form_spacing"
+                    placeholder="Username"
+                  />
+                  <input
+                    type="text"
+                    name="email"
+                    value={this.state.email}
+                    onChange={this.handleChange}
+                    className="form-control form_spacing"
+                    placeholder="Email"
+                  />
+                  <input
+                    type="text"
+                    name="city"
+                    value={this.state.city}
+                    onChange={this.handleChange}
+                    className="form-control form_spacing"
+                    placeholder="City"
+                  />
+                  <select
+                    name="state"
+                    value={this.state.state}
+                    onChange={this.handleChange}
+                    className="form-control form_spacing"
+                  >
+                    <StatesForm />
+                  </select>
+                  <p className="warning_text">{this.state.warning}</p>
+                </form>
               </React.Fragment>
             ) : (
               <React.Fragment>
@@ -166,7 +154,25 @@ class ProfileModal extends Component<ProfileModalProps, ProfileModalState> {
           </React.Fragment>
         </Modal.Body>
         <Modal.Footer>
-          <button className="btn btn-warning text_spacing">Edit Account</button>
+          {this.state.editing ? (
+            <button
+              className="btn btn-success text-spacing"
+              onClick={this.handleSubmit}
+            >
+              Submit{" "}
+              {this.state.loading && (
+                <Spinner animation="border" variant="light" size="sm" />
+              )}
+            </button>
+          ) : (
+            <button
+              className="btn btn-warning text_spacing"
+              onClick={this.beginEditing}
+            >
+              Edit Account
+            </button>
+          )}
+
           <button className="btn btn-danger text_spacing">
             Delete Account
           </button>
