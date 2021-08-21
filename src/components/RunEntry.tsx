80577d9aebdd71a0506d8ff87ae06fd6583070e5
Matthew Meyerink <meyerink@umich.edu>
2021-08-21 18:32:38 -0400

    Add method to compare run to training day
    Adds the did run meet trianing plan method to check if a run met the training plan requirement for the active plan.

--- src/components/RunEntry.tsx
+++ src/components/RunEntry.tsx
@@ -23,6 +23,8 @@ interface RunEntryState {
   state?: string;
   notes?: string;
   formError?: string;
+  activePlanData?: any;
+  activePlanExists?: boolean;
 }
 
 /**
@@ -39,12 +41,42 @@ class RunEntry extends Component<RunEntryProps, RunEntryState> {
       city: this.props.city,
       state: this.props.state,
       notes: "",
+      activePlanData: {},
+      activePlanExists: false,
 
       formError: "",
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    // Prepare headers for the request
+    const myHeaders = new Headers({
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + this.props.userData.token,
+    });
+
+    // Only send the request of the person has an active plan set
+    if (this.props.userData.active_plan !== -1) {
+      fetch(
+        Config.rpAPI +
+          `/training_plans/custom_plan/${this.props.userData.active_plan}`,
+        {
+          method: "GET",
+          headers: myHeaders,
+        }
+      )
+        .then((response) => response.json())
+        .then((data) =>
+          this.setState({
+            activePlanData: data,
+            activePlanExists: true,
+          })
+        )
+        .catch((error) => console.error(error));
+    }
   }
 
   handleChange(event: any) {
@@ -119,6 +151,108 @@ class RunEntry extends Component<RunEntryProps, RunEntryState> {
         }
       })
       .catch((error) => console.error(error));
+  }
+
+  /**
+   * Determines if the run satisfied the distance specified in the user's active plan
+   * @param date Date the run occured on
+   * @param runDistance Distance covered in the run
+   * @returns If the run satisfied a training plan run
+   */
+  didRunMeetPlanDistance(date: string, runDistance: number): boolean {
+    // Pull the active plan from state and create a week by week array
+    const activePlan = this.state.activePlanData.plan;
+    const activePlanArr = activePlan.split("-");
+
+    // Get the start date of the first week and the last week
+    const firstWeekStart = activePlanArr[0].split(",")[0];
+    const lastWeekStart =
+      activePlanArr[activePlanArr.length - 1].split(",")[0];
+
+    // Split dates in array to use in creating moment objects
+    const firstWeekStartArr = firstWeekStart.split("/");
+    const lastWeekStartArr = lastWeekStart.split("/");
+
+    // Create moment objects with first and last week moments
+    const firstWeekStartMoment = moment({
+      year: parseInt(firstWeekStartArr[2]),
+      month: firstWeekStartArr[0] - 1,
+      day: parseInt(firstWeekStartArr[1]),
+    });
+    firstWeekStartMoment.subtract(1, "days");
+    const lastWeekStartMoment = moment({
+      year: parseInt(lastWeekStartArr[2]),
+      month: lastWeekStartArr[0] - 1,
+      day: parseInt(lastWeekStartArr[1]),
+    });
+    lastWeekStartMoment.add(7, "days");
+
+    // Create a moment with the passed in run date
+    const dateMoment = moment(date, "YYYY-MM-DD");
+
+    // Check if today is within the range of the active plan dates
+    if (
+      dateMoment.isAfter(firstWeekStartMoment) &&
+      dateMoment.isBefore(lastWeekStartMoment)
+    ) {
+      for (let i = 0; i < activePlanArr.length; i++) {
+        // Split up the string of the current week
+        const currentWeekArr = activePlanArr[i].split(",");
+
+        // Gather the start date for the week and put it into an array
+        const weekStartDate = currentWeekArr[0];
+        const weekStartDateArr = weekStartDate.split("/");
+
+        // Create moments for the start of the week and for the end of the week
+        const weekStartDateMoment = moment({
+          year: parseInt(weekStartDateArr[2]),
+          month: weekStartDateArr[0] - 1,
+          day: weekStartDateArr[1],
+        });
+        weekStartDateMoment.subtract(1, "days");
+        const weekEndDateMoment = moment({
+          year: parseInt(weekStartDateArr[2]),
+          month: weekStartDateArr[0] - 1,
+          day: parseInt(weekStartDateArr[1]),
+        });
+        weekEndDateMoment.add(7, "days");
+
+        // Check if todays run is within this week
+        if (
+          moment().isAfter(weekStartDateMoment) &&
+          moment().isBefore(weekEndDateMoment)
+        ) {
+          weekStartDateMoment.add(1, "days");
+
+          // Possible easier way to do this. Determine which day of the week today is and pull that
+          // value from the plan array
+          let j = 1;
+          while (j < 8) {
+            // Pull the ints for the current day, month, year
+            const currentDay = moment().day();
+            const currentMonth = moment().month();
+            const currentYear = moment().year();
+
+            // Pull the ints for the current day in the plan's date, month, year
+            const planDay = weekStartDateMoment.day();
+            const planMonth = weekStartDateMoment.month();
+            const planYear = weekStartDateMoment.year();
+
+            if (
+              currentDay === planDay &&
+              currentMonth === planMonth &&
+              currentYear === planYear
+            ) {
+              return runDistance >= currentWeekArr[j];
+            }
+            weekStartDateMoment.add(1, "days");
+            j++;
+          }
+        }
+      }
+    }
+
+    return false;
   }
 
   render() {
